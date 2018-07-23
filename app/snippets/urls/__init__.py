c14247f25205afc28453c433c068aeb02fb0dcd5
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-23 17:13:49 +0900

    snippet_list, snippet_detail 메소드 mixins 기법으로 다시 구현

--- app/snippets/urls/__init__.py
+++ app/snippets/urls/__init__.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 
-from . import django_view, api_view
+from . import django_view, api_view, mixins
 
 app_name = 'snippets'
 
 urlpatterns = [
     path('django-view/', include(django_view)),
-    path('api-view/', include(api_view))
+    path('mixins/', include(mixins)),
+    path('api-view/', include(api_view)),
 ]
