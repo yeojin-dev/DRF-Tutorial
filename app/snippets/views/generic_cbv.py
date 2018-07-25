1477b630f8da7c0b4301c8e5a935073b878c6ae3
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-25 20:28:37 +0900

    SnippetList 메소드에 Pagination 적용

--- app/snippets/views/generic_cbv.py
+++ app/snippets/views/generic_cbv.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
 
+from utils.paginations import SnippetListPagination
 from ..models import Snippet
 from ..serializers import (
     UserListSerializer,
@@ -21,6 +22,7 @@ __all__ = (
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    pagination_class = SnippetListPagination
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
