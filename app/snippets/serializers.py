d212f1cab3f000b4d6554297f043980f80418ff5
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-25 14:47:01 +0900

    UserSerializer 추가

--- app/snippets/serializers.py
+++ app/snippets/serializers.py
@@ -1,8 +1,31 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+from .models import Snippet
+
+User = get_user_model()
+
+__all__ = (
+    'UserSerializer',
+    'SnippetSerializer',
+)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'snippets',
+        ]
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    # ReadOnlyField - 입력은 JSON 외부에서 받지만(유효성 검사 제외) 읽어들일 때는 필요함
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Snippet
@@ -13,4 +36,10 @@ class SnippetSerializer(serializers.ModelSerializer):
             'linenos',
             'language',
             'style',
+            'owner',
         ]
+
+        # 아래와 같이 설정하면 JSON에는 id가 나옴 - owner가 외래키이기 때문
+        # read_only_fields = [
+        #     'owner',
+        # ]
