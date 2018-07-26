948d60c46c23f662e65fae69af63fa9dcf3aa3f5
Yeojin Kim <yeojin.dev@gmail.com>
2018-07-26 15:25:22 +0900

    viewsets, routers 기법으로 다시 구현

--- app/snippets/urls/__init__.py
+++ app/snippets/urls/__init__.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from . import django_view, api_view, mixins, generic_cbv
+from . import django_view, api_view, mixins, generic_cbv, viewsets_router
 
 app_name = 'snippets'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('mixins/', include(mixins)),
     path('api-view/', include(api_view)),
     path('generic-cbv/', include(generic_cbv)),
+    path('viewsets-router/', include(viewsets_router)),
 ]
